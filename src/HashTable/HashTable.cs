5c66e647e292281d0c10d2bbcfdd565bea0fcbe9
Diego Malta <diego_malta@hotmail.com>
2020-03-27 22:52:06 -0500

    Add hashTable

---
+++ src/HashTable/HashTable.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HashTable
+{
+    public class KeyValuePair
+    {
+        public int Key { get; set; }
+        public string Value { get; set; }
+    }
+
+    public class HashTable
+    {
+        public LinkedList<KeyValuePair>[] hashCollection;
+
+        public HashTable()
+        {
+            hashCollection = new LinkedList<KeyValuePair>[5];
+        }
+
+        public void Put(int key, string value)
+        {
+
+            var bucket = GetOrCreateBuket(key);
+
+            if (FindItem(bucket, key) != null)
+                return;
+
+            bucket.AddLast(new KeyValuePair { Key = key, Value = value });
+        }
+
+        public string Get(int key)
+        {
+            return FindItem(GetBucket(key), key)?.Value;
+        }
+
+        public void Remove(int key)
+        {
+            var bucket = GetBucket(key);
+
+            var item = FindItem(bucket, key);
+            if (item != null)
+                bucket.Remove(item);
+        }
+
+        private LinkedList<KeyValuePair> GetBucket(int key)
+        {
+            int hash = GetHash(key);
+            return hashCollection[hash];
+        }
+
+        private LinkedList<KeyValuePair> GetOrCreateBuket(int key)
+        {
+            int hash = GetHash(key);
+            if (hashCollection[hash] == null)
+                hashCollection[hash] = new LinkedList<KeyValuePair>();
+            return hashCollection[hash];
+        }
+
+        private KeyValuePair FindItem(LinkedList<KeyValuePair> bucket, int key)
+        {
+            if(bucket != null)
+            {
+                foreach (var item in bucket)
+                {
+                    if (item.Key == key)
+                        return item;
+                }
+            }            
+            return null;
+        }
+
+        private int GetHash (int key)
+        {
+            return key % hashCollection.Length;
+        }
+    }
+}
---
+++ src/HashTable/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace HashTable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HashTable ht = new HashTable();
+            ht.Put(1, "a");
+            ht.Put(2, "b");
+            ht.Put(4, "c");
+            ht.Put(5, "d");
+            ht.Put(6, "e");
+            ht.Put(7, "f");
+
+            Console.WriteLine(ht.Get(4));
+
+            ht.Remove(5);
+            ht.Remove(10);
+        }
+    }
+}
