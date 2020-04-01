021c5792e0234969c81845ffbd4cbf08bcfc0986
Diego Malta <diego_malta@hotmail.com>
2020-03-31 20:13:51 -0500

    SpiralArray

---
+++ src/SpiralArray/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace SpiralArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SpiralArray spiral = new SpiralArray();
+            var result = spiral.GetSpiral(4);
+
+            for(int r = 0; r < result.GetLength(1); r++)
+            {
+                for (int c = 0; c < result.GetLength(0); c++)
+                    Console.Write($" {result[r, c]} ");
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ src/SpiralArray/SpiralArray.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpiralArray
+{
+    public class SpiralArray
+    {
+        public int [,] GetSpiral(int lenght)
+        {
+            int[,] spiralArray = new int[lenght, lenght];
+            string direction = "left-right";
+            int count = 1;
+            int rounds = 0;
+
+            while (count <= lenght * lenght)
+            { 
+                if (direction.Equals("left-right"))  {
+                    for (int y = 0 + rounds; y < lenght - rounds; y++)
+                    {
+                        spiralArray[0 + rounds, y] = count++;
+                    }
+                    direction = "up-down";
+                } 
+                if (direction.Equals("up-down"))  {
+                    for (int x = 1 + rounds; x < lenght - rounds; x++)
+                    {
+                        spiralArray[x, lenght - 1 - rounds] = count++;                        
+                    }
+                    direction = "right-left";
+                }
+                if (direction.Equals("right-left"))  {
+                    for (int y = lenght - rounds - 2; y >= 0 + rounds; y--)
+                    {
+                        spiralArray[lenght - 1 - rounds, y] = count++;
+                    }
+                    direction = "down-up";
+                }
+                if (direction.Equals("down-up")) {
+                    for (int x = lenght - rounds - 2; x >= 1 + rounds; x--)
+                    {
+                        spiralArray[x, 0 + rounds] = count++;
+                    }
+                    direction = "left-right";
+                    rounds++;
+                }
+            }
+
+            return spiralArray;
+        }
+    }
+}
