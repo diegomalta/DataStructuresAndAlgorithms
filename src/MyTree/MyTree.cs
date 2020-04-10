2fee460a3fa9d13cbefbaf1ed05c9652571b3491
Diego Malta <diego_malta@hotmail.com>
2020-04-10 10:36:18 -0500

    add the tree project

---
+++ src/MyTree/MyTree.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyTree
+{
+    public class MyTree
+    {
+        public class Node
+        {
+            public int Value { get; set; }
+            public Node Rigth { get; set; }
+            public Node Left { get; set; }
+
+            public Node(int value)
+            {
+                Value = value;
+            }
+
+        }
+
+        private Node Root;
+
+        public MyTree()
+        {
+        }
+
+        public void Add(int value)
+        {
+            var newNode = new Node(value);
+            if(Root == null)
+            {
+                Root = newNode;
+                return;
+            }
+
+            var current = Root;
+            while (true)
+            {
+                if (current.Value > value)
+                {
+                    if (current.Left == null)
+                    {
+                        current.Left = newNode;
+                        break;
+                    }
+                    current = current.Left;
+                }
+                else
+                {
+                    if (current.Rigth == null)
+                    {
+                        current.Rigth = newNode;
+                        break;
+                    }
+                    current = current.Rigth;
+                }
+            }
+        }
+
+        public bool Find (int value)
+        {
+            return true;
+        }
+    }
+}
---
+++ src/MyTree/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+
+namespace MyTree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var tree = new MyTree();
+            tree.Add(10);
+            tree.Add(5);
+            tree.Add(15);
+            tree.Add(6);
+            tree.Add(1);
+            tree.Add(8);
+            tree.Add(12);
+            tree.Add(18);
+            tree.Add(17);
+
+        }
+    }
+}
