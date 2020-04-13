c487db1d5041bb088caa33eb91c11fe1609b31fb
Diego Malta <diego_malta@hotmail.com>
2020-04-13 09:53:17 -0500

    add avl tree

---
+++ src/MyAvlTree/MyAvlTree.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyAvlTree
+{
+    public class MyAvlTree
+    {
+        class Node
+        {
+            public int Value;
+            public Node Left;
+            public Node Right;
+            public int Height;
+
+            public Node(int value)
+            {
+                Value = value;
+            }
+        }
+
+        private Node Root;
+
+        public void Insert(int value)
+        {
+            Root = Insert(Root, value);
+        }
+
+        private Node Insert(Node root, int value)
+        {
+            if (root == null)
+                return new Node(value);
+
+            if (value > root.Value)
+                root.Right = Insert(root.Right, value);
+            else
+                root.Left = Insert(root.Left, value);
+
+            root.Height = Math.Max(NodeHeight(root.Left),
+                                   NodeHeight(root.Right)) + 1;
+
+            var balanceFactor = GetBalanceFactor(root);
+            if (IsLeftHeavy(root))
+                Console.WriteLine($"Left - Node: {root.Value} Factor: {balanceFactor}");
+            else if (IsRightHeavy(root))
+            {
+                Console.WriteLine($"Right - Node: {root.Value} Factor: {balanceFactor}");
+                balanceFactor = GetBalanceFactor(root.Right);
+            }
+                
+
+            return root;
+        }
+
+        private bool IsLeftHeavy(Node root)
+        {
+            return GetBalanceFactor(root) > 1;
+        }
+
+        private bool IsRightHeavy(Node root)
+        {
+            return GetBalanceFactor(root) < -1;
+        }
+
+        private int GetBalanceFactor(Node root)
+        {
+            return root == null ? 0 : NodeHeight(root.Left) - NodeHeight(root.Right);
+        }
+
+        private int NodeHeight(Node node)
+        {
+            return node != null ? node.Height : -1;
+        }
+    }
+}
---
+++ src/MyAvlTree/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace MyAvlTree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var avlTree = new MyAvlTree();
+            avlTree.Insert(10);
+            avlTree.Insert(30);
+            avlTree.Insert(20);
+            
+            
+        }
+    }
+}
