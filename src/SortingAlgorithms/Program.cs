8dc2013c3ce303e172f466bc9484da58bf1b5263
Diego Malta <diego_malta@hotmail.com>
2020-04-20 23:53:58 -0500

    adding the sorting project

---
+++ src/SortingAlgorithms/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace SortingAlgorithms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var array = new int[] { 6, 2, 9, 4, 5 };
+            BubbleSort(array);
+            Console.WriteLine(string.Join(",", array));
+        }
+
+        static void BubbleSort(int[] array)
+        {
+            for (var i = 0; i < array.Length; i++)
+            {
+                var isSorted = true;
+                for (var j = 1; j < array.Length - i; j++)
+                {
+                    if (array[j - 1] > array[j])
+                    {
+                        var temp = array[j - 1];
+                        array[j - 1] = array[j];
+                        array[j] = temp;
+                        isSorted = false;
+                    }
+                }
+                if (isSorted)
+                    break;
+            }
+        }
+    }
+}
