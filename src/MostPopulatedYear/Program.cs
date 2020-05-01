2e54e47f11cadc96d7f3a126e88acb47ef2783a0
Diego Malta <diego_malta@hotmail.com>
2020-05-01 10:59:07 -0500

    agg most populated year

---
+++ src/MostPopulatedYear/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MostPopulatedYear
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = new List<Tuple<int, int>>();
+            input.Add(new Tuple<int, int>(2000, 2010));
+            input.Add(new Tuple<int, int>(1975, 2005));
+            input.Add(new Tuple<int, int>(1975, 2003));
+            input.Add(new Tuple<int, int>(1803, 1809));
+            input.Add(new Tuple<int, int>(1750, 1869));
+            input.Add(new Tuple<int, int>(1840, 1935));
+            input.Add(new Tuple<int, int>(1803, 1921));
+            input.Add(new Tuple<int, int>(1894, 1921));
+
+
+
+        }
+    }
+}
