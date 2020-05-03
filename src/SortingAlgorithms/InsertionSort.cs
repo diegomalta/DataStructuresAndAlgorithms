9723f8f4437563126a41b7ac47decaf5c668103b
Diego Malta <diego_malta@hotmail.com>
2020-05-03 16:18:19 -0500

    insertion Sort

---
+++ src/SortingAlgorithms/InsertionSort.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortingAlgorithms
+{
+    public class InsertionSort
+    {
+        public void Sort(int[] array)
+        {
+            for (var i = 1; i < array.Length; i++)
+            {
+                InsertItem(array, array[i], i);
+            }
+        }
+
+        private void InsertItem(int[] array, int value, int bound)
+        {
+            var i = bound;
+            var index = -1;
+            while (i > 0 && value < array[i - 1])
+            {
+                array[i] = array[i - 1];
+                index = i - 1;
+                i--;
+            }               
+            if (index >= 0)
+                array[index] = value;
+        }
+    }
+}
--- src/SortingAlgorithms/Program.cs
+++ src/SortingAlgorithms/Program.cs
@@ -10,10 +10,12 @@ namespace SortingAlgorithms
             var array = new int[] { 6, 2, 9, 4, 5 };
             var bubbleSort = new BubbleSort();
             var selectionSort = new SelectionSort();
+            var insertionSort = new InsertionSort();
 
             Console.WriteLine("NoSorted: " + string.Join(",", array));
             //bubbleSort.Sort(array);
-            selectionSort.Sort(array);
+            //selectionSort.Sort(array);
+            insertionSort.Sort(array);
             Console.WriteLine("Sorted: " + string.Join(",", array));
         }
 
